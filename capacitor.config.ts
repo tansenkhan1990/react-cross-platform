b31de827cac220d13a0532a4ac10a9780e804aee
tansenkhan1990 <tansenkhan1990@gmail.com>
2025-06-29 11:43:24 +0200

    add capacitor.js

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'react-cross-platform',
+  webDir: 'build'
+};
+
+export default config;
