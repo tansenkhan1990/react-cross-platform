414cfc5623ef974972dc5fc09d859b91dd637367
tansenkhan1990 <tansenkhan1990@gmail.com>
2025-06-29 23:31:22 +0200

    add changes

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,18 +1,18 @@
-// src/layouts/PrivateLayout.tsx
 import React from "react";
 import { Outlet } from "react-router-dom";
 import PrivateHeader from "../components/PrivateHeader";
 import Footer from "../components/Footer";
 
-const PrivateLayout = () => (
-    
-  <div className="min-h-screen flex flex-col bg-white text-gray-800">
-    <PrivateHeader />
-    <main className="flex-grow">
-      <Outlet />
-    </main>
-    <Footer />
-  </div>
-);
+const PrivateLayout = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-white text-gray-800">
+      <PrivateHeader />
+      <main className="flex-grow flex flex-col justify-center">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default PrivateLayout;
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,14 +1,17 @@
-// src/layouts/PublicLayout.tsx
 import React from "react";
 import PublicHeader from "../components/PublicHeader";
 import Footer from "../components/Footer";
 
-const PublicLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="min-h-screen flex flex-col bg-white text-gray-800">
-    <PublicHeader />
-    <main className="flex-grow">{children}</main>
-    <Footer />
-  </div>
-);
+const PublicLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen flex-col bg-white text-gray-800">
+      <PublicHeader />
+      <main className="flex-grow flex flex-col justify-center">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default PublicLayout;
